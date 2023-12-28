8ff5fa8565ce488566abd1d1977715ceb445e7b7
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-28 00:00:50 -0600

    styled home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,20 +1,27 @@
 export function HomePage() {
   return (
-    <div>
+    <div className="bg-[#B7B7B7] min-h-screen w-full">
       <div className="min-h-60 bg-gradient-to-r from-[#B31312] to-[#FF7676]">
-        <h1 className="text-3xl font-bold text-[#FCF5ED] flex justify-start pl-10 pt-10">Welcome to Roll 20</h1>
+        <h1 className="text-3xl font-bold text-[#FCF5ED] flex justify-start pl-10 pt-10">Welcome to Professor Roll</h1>
         <p className="flex justify-start pl-10 ml-5 ">Dungeons and Dragons application</p>
       </div>
-      <div>
-        <div className="absolute top-50 right-0 bg-gradient-to-l from-black h-[49%] w-[100%] z-20">
-          <p className="flex justify-end p-5 text-white">hello</p>
+      <div className="flex justify-start ">
+        <div className="pl-[20px] bg-[url('/public/calm.jpeg')] overflow-hidden bg-no-repeat w-screen h-screen"></div>
+        <div className="absolute bg-gradient-to-r from-black border-[5px] border-black  w-[100%] h-[600px] z-10">
+          <p className="absolute pl-3 pt-4 text-white z-3 max-w-[50%] font-bold text-xl">Welcome to Prof. Roll!</p>
+          <p className="absolute pl-3 pt-[70px] text-white z-3 max-w-[800px] text-left text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat perferendis perspiciatis nemo voluptates
+            id non, maxime quidem saepe in, quo aut omnis voluptatum atque nisi quia facilis a ab ut! Lorem ipsum dolor
+            sit amet, consectetur adipisicing elit. Quaerat perferendis perspiciatis nemo voluptates id non, maxime
+            quidem saepe in, quo aut omnis voluptatum atque nisi quia facilis a ab ut! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Quaerat perferendis perspiciatis nemo voluptates id non, maxime quidem saepe
+            in, quo aut omnis voluptatum atque nisi quia facilis a ab ut! Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Quaerat perferendis perspiciatis nemo voluptates id non, maxime quidem saepe in, quo aut
+            omnis voluptatum atque nisi quia facilis a ab ut! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quaerat perferendis perspiciatis nemo voluptates id non, maxime quidem saepe in, quo aut omnis voluptatum
+            atque nisi quia facilis a ab ut!
+          </p>
         </div>
-        <img
-          className="bg-gradient-to-r border-2 border-black z-10"
-          src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/08/dnd-5e-dragonlance-chapter-2-art-1.jpg"
-          alt="Dnd img"
-        />
-        <p></p>
       </div>
     </div>
   );
