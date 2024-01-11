8dcf80565ad84bbab2c42dbae7a3dcd5277a2cf9
ben <ben.shin27@gmail.com>
2024-01-11 12:38:18 -0600

    added combatshow

---
+++ src/components/MainContent/Combat/CombatShow.jsx
@@ -0,0 +1,53 @@
+export function CombatShow(props) {
+  const maxLengthCheck = (object) => {
+    if (object.target.value.length > object.target.maxLength) {
+      object.target.value = object.target.value.slice(0, object.target.maxLength);
+    }
+  };
+  return (
+    <div>
+      <form>
+        <div className="grid grid-cols-2">
+          <input type="text" name="tab_id" defaultValue={1} hidden />
+          <div>
+            <label htmlFor="creature_name">Name:</label>
+            <input type="text" name="creature_name" defaultValue={props.combat.creature_name} />
+          </div>
+          <div>
+            <label htmlFor="initiative_roll">Initiative:</label>
+            <input
+              type="number"
+              maxLength="2"
+              onChange={maxLengthCheck}
+              name="initiative_roll"
+              defaultValue={props.combat.initiative_roll}
+            />
+          </div>
+          <div>
+            <label htmlFor="health">Health:</label>
+            <input
+              type="number"
+              name="health"
+              maxLength={3}
+              onChange={maxLengthCheck}
+              defaultValue={props.combat.health}
+            />
+          </div>
+          <div>
+            <label htmlFor="status">Status:</label>
+            <select name="status" defaultValue={props.combat.status}>
+              <option value="" selected disabled hidden>
+                Choose Status
+              </option>
+              <option value="In Combat">In Combat</option>
+              <option value="Out of Combat">Out of Combat</option>
+              <option value="Incapacitated">incapacitated</option>
+              <option value="Saving Throw">Saving Throw</option>
+              <option value="Dead">Dead</option>
+            </select>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/components/MainContent/Combat/Initiative.jsx
+++ src/components/MainContent/Combat/Initiative.jsx
@@ -2,10 +2,31 @@ import { useState, useEffect } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import axios from "axios";
+import { Modal } from "../../Modal/Modal";
+import { CombatShow } from "./CombatShow";
 
 export function Initiative() {
   const [creatures, setCreatures] = useState([]);
+  const [isCreatureUpdateVisible, setIsCreatureUpdateVisible] = useState(false);
+  const [combat, setCombat] = useState([]);
+
+  const handleShowUpdate = (creature) => {
+    console.log(creature);
+    setIsCreatureUpdateVisible(true);
+    setCombat(creature);
+  };
+
+  const handleClose = () => {
+    setIsCreatureUpdateVisible(false);
+  };
+
   let tabId = 0;
+
+  const maxLengthCheck = (object) => {
+    if (object.target.value.length > object.target.maxLength) {
+      object.target.value = object.target.value.slice(0, object.target.maxLength);
+    }
+  };
 
   const combatNew = (params, successCallback) => {
     axios.post("http://localhost:3000/combats.json", params).then((response) => {
@@ -19,29 +40,153 @@ export function Initiative() {
       setCreatures(response.data);
     });
   };
+
+  const handleDestroyCombat = (creature) => {
+    axios.destroy(`http://localhost:3000/combats/${creature.id}.json`).then((response) => {
+      setCharacters(characters.filter((c) => c.id !== character.id));
+    });
+  };
+
+  const handleUpdateCombat = (creature) => {
+    axios.destroy;
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
     combatNew(params, () => event.target.reset());
     ref.current.value = "";
   };
-
-  useEffect(handleIndexCombat, []);
+  if (creatures) {
+    useEffect(handleIndexCombat, []);
+  }
   return (
     <>
       <div>
         <Tabs>
           <TabList>
-            <Tab>Tab 1</Tab>
-            <Tab>Tab 2</Tab>
-            <Tab>Tab 3</Tab>
-            <Tab>Tab 4</Tab>
-            <Tab>Tab 5</Tab>
+            <Tab onClick={handleIndexCombat}>Tab 1</Tab>
+            <Tab onClick={handleIndexCombat}>Tab 2</Tab>
+            <Tab onClick={handleIndexCombat}>Tab 3</Tab>
+            <Tab onClick={handleIndexCombat}>Tab 4</Tab>
+            <Tab onClick={handleIndexCombat}>Tab 5</Tab>
           </TabList>
           {/* tab 1 */}
           <TabPanel>
             {(tabId = 1)}
-            <h2>Any content 1</h2>
+            {creatures.map((creature) =>
+              creature.tab_id == tabId ? (
+                <div key={creature.id}>
+                  <div className="flex justify-center">
+                    <div className="flex justify-center my-5 w-[80%] h-9 bg-[#A9A9A9] rounded-md">
+                      <p className="capitalize pt-1 px-2">{creature.creature_name}</p>
+                      <p className="pt-1 px-2">Roll: {creature.initiative_roll}</p>
+                      <p className="pt-1 px-2">Health: {creature.health}</p>
+                      <p className="pt-1 px-2">Status: {creature.status}</p>
+                      <button onClick={() => handleShowUpdate(creature)}>Update</button>
+                      <Modal show={isCreatureUpdateVisible} onClose={handleClose}>
+                        <CombatShow combat={combat} />
+                      </Modal>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <></>
+              )
+            )}
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div className="flex justify-center">
+                  <input type="text" name="tab_id" value={1} hidden />
+                  <div>
+                    <label htmlFor="creature_name">Name:</label>
+                    <input type="text" name="creature_name" />
+                  </div>
+                  <div>
+                    <label htmlFor="initiative_roll">Initiative:</label>
+                    <input type="number" maxLength="2" onChange={maxLengthCheck} name="initiative_roll" />
+                  </div>
+                  <div>
+                    <label htmlFor="health">Health:</label>
+                    <input type="number" name="health" maxLength={3} onChange={maxLengthCheck} />
+                  </div>
+                  <div>
+                    <label htmlFor="status">Status:</label>
+                    <select name="status">
+                      <option value="" selected disabled hidden>
+                        Choose Status
+                      </option>
+                      <option value="In Combat">In Combat</option>
+                      <option value="Out of Combat">Out of Combat</option>
+                      <option value="Incapacitated">incapacitated</option>
+                      <option value="Saving Throw">Saving Throw</option>
+                      <option value="Dead">Dead</option>
+                    </select>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="text-[#FF6969] px-1 border-2 border-[#FF6969] rounded-lg bg-[#FFE5CA] hover:bg-[#FF6969] hover:text-[#FFE5CA] hover:duration-200 mr-2"
+                >
+                  Add
+                </button>
+              </form>
+            </div>
+          </TabPanel>
+          {/* tab 2 */}
+          <TabPanel>
+            {(tabId = 2)}
+
+            {creatures.map((creature) =>
+              creature.tab_id == 2 ? (
+                <>
+                  <div key={creature.id}>
+                    <p>{creature.creature_name}</p>
+                    <p>{creature.initiative_roll}</p>
+                    <p>{creature.health}</p>
+                    <p>{creature.status}</p>
+                  </div>
+                </>
+              ) : (
+                <></>
+              )
+            )}
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input type="text" name="tab_id" value={2} hidden />
+                <div>
+                  <label htmlFor="creature_name">Name:</label>
+                  <input type="text" name="creature_name" />
+                </div>
+                <div>
+                  <label htmlFor="initiative_roll">Initiative:</label>
+                  <input type="text" name="initiative_roll" />
+                </div>
+                <div>
+                  <label htmlFor="health">Health:</label>
+                  <input type="text" name="health" />
+                </div>
+                <div>
+                  <label htmlFor="status">Status:</label>
+                  <select name="status">
+                    <option value="" selected disabled hidden>
+                      Choose Status
+                    </option>
+                    <option value="In Combat">In Combat</option>
+                    <option value="Out of Combat">Out of Combat</option>
+                    <option value="Incapacitated">incapacitated</option>
+                    <option value="Saving Throw">Saving Throw</option>
+                    <option value="Dead">Dead</option>
+                  </select>
+                </div>
+                <button type="submit">Add</button>
+              </form>
+            </div>
+          </TabPanel>
+          {/* tab 3 */}
+          <TabPanel>
+            {(tabId = 3)}
+
             {creatures.map((creature) =>
               creature.tab_id == tabId ? (
                 <>
@@ -87,49 +232,8 @@ export function Initiative() {
               </form>
             </div>
           </TabPanel>
-          {/* tab 2 */}
-          <TabPanel>
-            {(tabId = 2)}
-            <h2>Any content 2</h2>
-            {creatures.map((creature) =>
-              creature.tab_id == tabId ? (
-                <>
-                  <div key={creature.id}>
-                    <p>{creature.creature_name}</p>
-                    <p>{creature.initiative_roll}</p>
-                    <p>{creature.health}</p>
-                    <p>{creature.status}</p>
-                  </div>
-                </>
-              ) : (
-                <></>
-              )
-            )}
-            <div></div>
-          </TabPanel>
-          {/* tab 3 */}
-          <TabPanel>
-            {(tabId = 3)}
-            <h2>Any content 2</h2>
-            {creatures.map((creature) =>
-              creature.tab_id == tabId ? (
-                <>
-                  <div key={creature.id}>
-                    <p>{creature.creature_name}</p>
-                    <p>{creature.initiative_roll}</p>
-                    <p>{creature.health}</p>
-                    <p>{creature.status}</p>
-                  </div>
-                </>
-              ) : (
-                <></>
-              )
-            )}
-          </TabPanel>
           {/* tab 4 */}
           <TabPanel>
-            {(tabId = 4)}
-            <h2>Any content 2</h2>
             {creatures.map((creature) =>
               creature.tab_id == tabId ? (
                 <>
@@ -144,11 +248,41 @@ export function Initiative() {
                 <></>
               )
             )}
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input type="text" name="tab_id" value={1} hidden />
+                <div>
+                  <label htmlFor="creature_name">Name:</label>
+                  <input type="text" name="creature_name" />
+                </div>
+                <div>
+                  <label htmlFor="initiative_roll">Initiative:</label>
+                  <input type="text" name="initiative_roll" />
+                </div>
+                <div>
+                  <label htmlFor="health">Health:</label>
+                  <input type="text" name="health" />
+                </div>
+                <div>
+                  <label htmlFor="status">Status:</label>
+                  <select name="status">
+                    <option value="" selected disabled hidden>
+                      Choose Status
+                    </option>
+                    <option value="In Combat">In Combat</option>
+                    <option value="Out of Combat">Out of Combat</option>
+                    <option value="Incapacitated">incapacitated</option>
+                    <option value="Dead">Dead</option>
+                  </select>
+                </div>
+                <button type="submit">Add</button>
+              </form>
+            </div>
           </TabPanel>
           {/* tab 5 */}
           <TabPanel>
             {(tabId = 5)}
-            <h2>Any content 2</h2>
+
             {creatures.map((creature) =>
               creature.tab_id == tabId ? (
                 <>
@@ -163,6 +297,36 @@ export function Initiative() {
                 <></>
               )
             )}
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input type="text" name="tab_id" value={1} hidden />
+                <div>
+                  <label htmlFor="creature_name">Name:</label>
+                  <input type="text" name="creature_name" />
+                </div>
+                <div>
+                  <label htmlFor="initiative_roll">Initiative:</label>
+                  <input type="text" name="initiative_roll" />
+                </div>
+                <div>
+                  <label htmlFor="health">Health:</label>
+                  <input type="text" name="health" />
+                </div>
+                <div>
+                  <label htmlFor="status">Status:</label>
+                  <select name="status">
+                    <option value="" selected disabled hidden>
+                      Choose Status
+                    </option>
+                    <option value="In Combat">In Combat</option>
+                    <option value="Out of Combat">Out of Combat</option>
+                    <option value="Incapacitated">incapacitated</option>
+                    <option value="Dead">Dead</option>
+                  </select>
+                </div>
+                <button type="submit">Add</button>
+              </form>
+            </div>
           </TabPanel>
         </Tabs>
       </div>
